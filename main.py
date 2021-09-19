8ca424a423035c7e64886e1bbd49bf026c9a5602
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2021-09-19 01:22:12 +0100

    expose api endpoint to fetch stations

---
+++ main.py
@@ -0,0 +1,65 @@
+import uvicorn
+from typing import List
+from opencdms.models.climsoft.v4_1_1_core import Station
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import Session
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Set database url to mariadb_climsoft_db_v4 database url
+database_url = ""
+engine = create_engine(database_url)
+
+
+SessionLocal = sessionmaker(engine)
+
+
+
+class StationSchema(BaseModel):
+    """Station response schema"""
+    stationId: str
+    stationName: str
+    wmoid: str
+    icaoid: str
+    latitude: float
+    qualifier: str
+    longitude: float
+    elevation: str
+    geoLocationMethod: str
+    geoLocationAccuracy: float
+    openingDatetime: str
+    closingDatetime: str
+    country: str
+    authority: str
+    adminRegion: str
+    drainageBasin: str
+    wacaSelection: int
+    cptSelection: int
+    stationOperational: int
+
+
+
+def get_session():
+    """
+    Api dependency to provide database session to a request
+    """
+    session: Session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+    finally:
+        session.close()
+
+
+@app.get('/stations', response_model=List[StationSchema])
+def fetch_stations(session: Session = Depends(get_session)):
+    return session.query(Station).all()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
