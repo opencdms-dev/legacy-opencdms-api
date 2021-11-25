ac26884aa362189f2d35f0b47944ced77fc2ffff
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-11-26 02:43:08 +0600

    add synop feature schema

---
+++ src/apps/climsoft/schemas/synopfeature_schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, constr
+from typing import List
+from src.common_schemas import Response
+
+
+class CreateSynopFeature(BaseModel):
+    abbreviation: int
+    description: constr(max_length=255)
+
+
+class UpdateSynopFeature(BaseModel):
+    description: constr(max_length=255)
+
+
+class SynopFeature(CreateSynopFeature):
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+
+class SynopFeatureResponse(Response):
+    result: List[SynopFeature]
+
+
+
+
+
+
+
+
