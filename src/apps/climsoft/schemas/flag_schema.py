fa3a4ebbec0cb222fd129e887abe212d6dfe0477
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-11-25 23:04:05 +0600

    add field mapping to flag schema

--- src/apps/climsoft/schemas/flag_schema.py
+++ src/apps/climsoft/schemas/flag_schema.py
@@ -8,15 +8,31 @@ class CreateFlag(BaseModel):
     numSymbol: int
     description: constr(max_length=255)
 
+    class Config:
+        fields = {
+            "characterSymbol": "character_symbol",
+            "numSymbol": "num_symbol"
+        }
+
 
 class UpdateFlag(BaseModel):
     numSymbol: int
     description: constr(max_length=255)
 
+    class Config:
+        fields = {
+            "numSymbol": "num_symbol"
+        }
+
 
 class Flag(CreateFlag):
     class Config:
+        allow_population_by_field_name = True
         orm_mode = True
+        fields = {
+            "characterSymbol": "character_symbol",
+            "numSymbol": "num_symbol"
+        }
 
 
 class FlagResponse(Response):
