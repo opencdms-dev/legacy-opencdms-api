82c91f0f918760605c47aacd6bd7a56580753dd6
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-11-25 23:08:28 +0600

    add datagen

---
+++ tests/datagen/climsoft/flag.py
@@ -0,0 +1,17 @@
+import uuid
+import random
+import datetime
+from typing import Tuple
+from faker import Faker
+from src.apps.climsoft.schemas import flag_schema
+
+
+fake = Faker()
+
+
+def get_valid_flag_input():
+    return flag_schema.Flag(
+        characterSymbol=uuid.uuid4().hex,
+        numSymbol=random.randint(1000000, 10000000),
+        description=" ".join(fake.sentences())
+    )
