7cb9a08bab7edfd464cd4d46a4ac9c5ff5e0f75f
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-11-27 01:14:29 +0600

    add physucalfeatureclass schema

---
+++ src/apps/climsoft/schemas/physicalfeatureclass_schema.py
@@ -0,0 +1,54 @@
+from src.apps.climsoft.schemas import station_schema
+from pydantic import BaseModel, constr
+from src.common_schemas import Response
+from typing import List
+
+
+class CreatePhysicalFeatureClass(BaseModel):
+    featureClass: constr(max_length=255)
+    description: constr(max_length=255)
+    refersTo: constr(max_length=255)
+
+    class Config:
+        fields = {
+            "featureClass": "feature_class",
+            "refersTo": "refers_to"
+        }
+
+
+class UpdatePhysicalFeatureClass(BaseModel):
+    description: constr(max_length=255)
+    refersTo: constr(max_length=255)
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        fields = {
+            "refersTo": "refers_to"
+        }
+
+
+class PhysicalFeatureClass(BaseModel):
+    featureClass: constr(max_length=255)
+    description: constr(max_length=255)
+    refersTo: constr(max_length=255)
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        fields = {
+            "featureClass": "feature_class",
+            "refersTo": "refers_to"
+        }
+
+
+class PhysicalFeatureClassWithStation(PhysicalFeatureClass):
+    station: station_schema.Station
+
+
+class PhysicalFeatureClassResponse(Response):
+    result: List[PhysicalFeatureClass]
+
+
+class PhysicalFeatureClassWithStationResponse(Response):
+    result: List[PhysicalFeatureClassWithStation]
