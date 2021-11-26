a261a10375afb547090f6f0b4c90baff540a8bef
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-11-27 02:04:24 +0600

    station location history schema

---
+++ src/apps/climsoft/schemas/stationlocationhistory_schema.py
@@ -0,0 +1,88 @@
+from src.apps.climsoft.schemas import station_schema
+from pydantic import BaseModel, constr
+from src.common_schemas import Response
+from typing import List
+
+
+class CreateStationLocationHistory(BaseModel):
+    belongsTo: constr(max_length=255)
+    openingDatetime: str
+    stationType: constr(max_length=255)
+    geoLocationMethod: constr(max_length=255)
+    geoLocationAccuracy: float
+    closingDatetime: str
+    latitude: float
+    longitude: float
+    elevation: int
+    authority: constr(max_length=255)
+    adminRegion: constr(max_length=255)
+    drainageBasin: constr(max_length=255)
+
+    class Config:
+        fields = {
+            "belongsTo": "belongs_to",
+            "stationType": "station_type",
+            "geolocation_method": "geolocationMethod",
+            "openingDatetime": "opening_datetime",
+            "closingDatetime": "closing_datetime",
+            "adminRegion": "admin_region",
+            "drainageBasin": "drainage_basin"
+        }
+
+
+class UpdateStationLocationHistory(BaseModel):
+    stationType: constr(max_length=255)
+    geoLocationMethod: constr(max_length=255)
+    geoLocationAccuracy: float
+    closingDatetime: str
+    latitude: float
+    longitude: float
+    elevation: int
+    authority: constr(max_length=255)
+    adminRegion: constr(max_length=255)
+    drainageBasin: constr(max_length=255)
+
+    class Config:
+        fields = {
+            "stationType": "station_type",
+            "geolocation_method": "geolocationMethod",
+            "closingDatetime": "closing_datetime",
+            "adminRegion": "admin_region",
+            "drainageBasin": "drainage_basin"
+        }
+
+
+class StationLocationHistory(BaseModel):
+    stationType: constr(max_length=255)
+    geoLocationMethod: constr(max_length=255)
+    geoLocationAccuracy: float
+    closingDatetime: str
+    latitude: float
+    longitude: float
+    elevation: int
+    authority: constr(max_length=255)
+    adminRegion: constr(max_length=255)
+    drainageBasin: constr(max_length=255)
+
+    class Config:
+        fields = {
+            "belongsTo": "belongs_to",
+            "stationType": "station_type",
+            "geolocation_method": "geolocationMethod",
+            "openingDatetime": "opening_datetime",
+            "closingDatetime": "closing_datetime",
+            "adminRegion": "admin_region",
+            "drainageBasin": "drainage_basin"
+        }
+
+
+class StationLocationHistoryWithStation(StationLocationHistory):
+    station: station_schema.Station
+
+
+class StationLocationHistoryResponse(Response):
+    result: List[StationLocationHistory]
+
+
+class StationLocationHistoryWithStationResponse(Response):
+    result: List[StationLocationHistoryWithStation]
