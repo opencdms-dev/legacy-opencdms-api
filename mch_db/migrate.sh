2dcd56d43f9c7c4810c9256139498025e3df4976
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2022-01-03 21:31:25 +0600

    try executing mch migration using docker exec

--- mch_db/migrate.sh
+++ mch_db/migrate.sh
@@ -1,1 +1,3 @@
-mysql --user=root --password=password < /scripts/create_mch_english_basic_tables.sql
+mysqld --datadir=/var/lib/mysql --user=mysql
+#&
+#mysql --user=root --password=password < /scripts/create_mch_english_basic_tables.sql
