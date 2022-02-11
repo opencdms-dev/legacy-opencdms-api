3edd1671c0a17f8a0be822355313c9b72616e3d8
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-02-11 12:33:19 +0600

    add climsoft rbac middleware

---
+++ src/opencdms_api/climsoft_rbac_config.py
@@ -0,0 +1,152 @@
+required_role_lookup = {
+    "/climsoft/v1/file-upload/image": {
+        "post": ""
+    },
+    "/climsoft/v1/s3/image": {
+        "get": ""
+    },
+    "/climsoft/v1/acquisition-types": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/data-forms": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/fault-resolutions": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/feature-geographical-positions": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/flags": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/instruments": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/instrument-fault-reports": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/instrument-inspections": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/obselements": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/observation-finals": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/observation-initials": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/obs-schedule-class": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/paper-archives": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/paper-archive-definitions": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/physical-features": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/physical-feature-class": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/qc-status-definitions": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/qc-types": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/reg-keys": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/stations": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/station-elements": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/station-location-histories": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/station-qualifiers": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    },
+    "/climsoft/v1/synop-features": {
+        "post": "",
+        "get": "",
+        "put": "",
+        "delete": ""
+    }
+}
