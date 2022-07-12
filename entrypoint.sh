a9a38dfd959cbe40032538eafab267db09835ed0
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-07-12 13:49:26 +0600

    fix network mode for traefik

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 python surface/api/manage.py migrate
-python init_climsoft_db.py
 ./scripts/load_initial_surface_data.sh
 pygeoapi openapi generate /code/pygeoapi-config.yml >| /code/pygeoapi-openapi.yml
 exec $@
